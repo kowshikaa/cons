7c19923ab9f18a4a560ca84f524c532eebfaf95c
kowshikaa <kowshimano@gmail.com>
2016-07-16 15:09:26 +0530

    finish

---
+++ README.c
@@ -0,0 +1,17 @@
+# cons
+#include<stdio.h>
+ #include<conio.h>
+void main()
+ {
+ char ch;
++scanf("%c",&ch);
+ if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+ {
+ printf("it is a vowel");
+ }
+ else
+ {
+ printf("it is a consonant");
+ }
+ getch();
+ }
